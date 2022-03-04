a775ee58bbfbce8246bf521e4c8ef686fb502fc4
Cuke7 <cuke.dev@mail.com>
2022-03-04 12:57:12 +0100

    yes

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <!-- <v-app-bar app color="primary"> </v-app-bar> -->
+    <v-main>
+      <div v-for="index in days" :key="index">
+        <v-img :src="getUrl(index)" class="ma-2"> </v-img>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    days: 0,
+  }),
+
+  mounted() {
+    let oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    let firstDate = new Date(2022, 2, 4);
+    let secondDate = new Date();
+
+    this.days = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+
+    console.log(this.days);
+  },
+
+  methods: {
+    getUrl(index) {
+      let url = "/photos/pic (" + index + ").JPG";
+      return url;
+    },
+  },
+};
+</script>
